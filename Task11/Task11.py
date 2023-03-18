e70c8e5eaee23a42650fb2dca025637637db12f5
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-03-18 19:51:02 +0700

    Added task 11. Not completed

--- Task11/Task11.py
+++ Task11/Task11.py
@@ -5,3 +5,19 @@
 # является числом Фибоначчи, выведите число -1.
 # Input: 5
 # Output: 6
+
+n = int(input('Input a digit fib: '))
+a = 0
+b = 1
+counter = 1
+while(a <= n):
+    print(a)
+    if n == a:
+        print(f"digit is on {counter} position")
+        break
+    elif n < a:
+            print("Digit is not exist")
+    else:
+        a, b = b, a + b
+        counter += 1        
+        
