11b20c43b44db9bc10f22270c6f62510f4a09efa
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-03-26 11:23:01 +0700

    Added task19

--- Task19/Task19.py
+++ Task19/Task19.py
@@ -5,3 +5,11 @@
 # положительное число.
 # Input: [1, 2, 3, 4, 5] k = 3
 # Output: [4, 5, 1, 2, 3]
+
+array_length = input("input quantity: ")
+k = input("input shift: ")
+nums_array = [i for i in range(1, int(array_length)+1)]
+print(nums_array)
+for i in range(int(k)):
+    nums_array.insert(0, nums_array.pop())
+print(nums_array)
