826e68713e088e0646c2713333e561037e1245f3
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-03-26 11:07:25 +0700

    Added task17

--- Task17/Task17.py
+++ Task17/Task17.py
@@ -3,3 +3,12 @@
 # встречается различных чисел.
 # Input: [1, 1, 2, 0, -1, 3, 4, 4]
 # Output: 6
+
+import random
+
+my_list = [random.randint(1, 10) for _ in range(random.randint(5, 10))]
+print(my_list)
+my_set = set(my_list)
+print(my_set)
+length = len(my_set)
+print(length)
