de01447ce05fac990e931b68511453003edf2427
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-03-31 12:05:54 +0700

    Task24

--- Task24/Task24.py
+++ Task24/Task24.py
@@ -16,7 +16,7 @@ n = int( input( 'n = ' ) )
 lis = [ int(x) for x in input( '-> ' ).split() ]
 n = len(lis)
 lis = lis + lis[:2]
-ma = 0
+maximum = 0
 for i in range(n):
-    ma = max( ma, lis[i] + lis[i+1] + lis[i+2] )
-print(ma)
+    maximum = max( maximum, lis[i] + lis[i+1] + lis[i+2] )
+print(maximum)
