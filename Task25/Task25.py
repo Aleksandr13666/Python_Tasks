e21f982c9f3b03512d4b1b8de37e408cbe26907e
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-03-31 13:51:39 +0700

    Added task25

--- Task25/Task25.py
+++ Task25/Task25.py
@@ -7,3 +7,17 @@
 # Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 # Для решения данной задачи используйте функцию
 # .split()
+
+letters = "a a a b c a a d c d d"
+letters_dict = {}
+result = letters.split(" ")
+print(letters)
+print(letters_dict)
+for i in range(len(letters)):
+    if lettars_dict.get(letters[i]) == None:
+        letters_dict[letters[i]] = 0
+        result += f"{letters[i]}"
+    else:
+        letters_dict[letters[i]] += 1
+        result += f"{letters[i]}_{letters_dict[letters[i]]}"
+print(result)        
