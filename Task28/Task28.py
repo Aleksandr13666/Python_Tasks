4715459cf5ad9343962c509515709a53dcd61c82
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-03-31 14:22:29 +0700

    Complete task28

--- Task28/Task28.py
+++ Task28/Task28.py
@@ -5,3 +5,16 @@
 
 # 2 2
 #     4
+
+a = int(input(" input a: "))
+b = int(input(" input b: "))
+def sum(a, b):
+    a += 1
+    b -= 1
+    if b > 0:
+        return sum(a, b)
+    else:
+        return a
+ 
+ 
+print(sum(a, b))
