2746613f31e8104c2e40bc2b6d31332a283e4bf8
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-04-01 19:10:43 +0700

    Complete task30

--- Task30/Task30.py
+++ Task30/Task30.py
@@ -2,3 +2,7 @@
 # Её первый элемент, разность и количество элементов нужно ввести с клавиатуры. 
 # Формула для получения n-го члена прогрессии: an = a1 + (n-1) * d.
 # Каждое число вводится с новой строки.
+
+a1, n, d = map(int, input("Input first element, difference and quantity of elements: ").split())
+progressive = [a1 + (n - 1) * d for i in range(1, n+1)]
+print(*progressive)
