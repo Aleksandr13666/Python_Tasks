8cf1b1554a29ff3d5d076f853ffa81be67f42952
Aleksandr Ad <aleksandr-ad@narod.ru>
2023-04-15 18:22:48 +0700

    Fix 40 and 42

--- Task40/Task40.py
+++ Task40/Task40.py
@@ -1,5 +1,6 @@
 # Задача 40: Работать с файлом california_housing_train.csv, который находится в папке sample_data. 
 # Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population).
+# https://colab.research.google.com/drive/1g1SLv_VWn3z_kFEhDkB0Ik2RRBeg_lID#scrollTo=BRlhjFdImVYM
 
 # df[df['population']<501]['median_house_value'].agg(['mean'])
 
--- Task42/Task42.py
+++ Task42/Task42.py
@@ -1,4 +1,5 @@
 # Задача 42: Узнать какая максимальная households в зоне минимального значения population.
+# https://colab.research.google.com/drive/1g1SLv_VWn3z_kFEhDkB0Ik2RRBeg_lID#scrollTo=BRlhjFdImVYM
 
 # df[df['population']==df['population'].min()]['households'].max()
 
